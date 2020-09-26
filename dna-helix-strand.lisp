5d2ad0045abbbbfa30bcdacc68f51b68016d08f5
Durham <durham.smith@gmail.com>
2020-09-26 21:19:45 +0200

    Add make-helix-strand

--- dna-helix-strand.lisp
+++ dna-helix-strand.lisp
@@ -4,3 +4,10 @@
   ()
   (:documentation "A CHEM-OBJ representing a DNA double helix strand"))
 
+
+
+
+
+(defun make-helix-strand (va vbb len &opt tfms)
+  "Returns a DNA-HELIX-STRAND CHEM-OBJ with len NUCLEOTIDE subobjs"
+  (make-instance 'dna-helix-strand :v-axis va :v-bb vbb :len len :tfms tfms))
