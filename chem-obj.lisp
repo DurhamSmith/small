812bd687fbff46f37cc67c63735786b52c10014f
Durham <durham.smith@gmail.com>
2020-09-26 18:16:48 +0200

    move existing classes to defclass/std

--- chem-obj.lisp
+++ chem-obj.lisp
@@ -1,11 +1,9 @@
 (in-package :small)
+;(describe 'chem-obj)
 
-(defclass chem-obj ()
-  ((subobjs :initarg :subobjs
-	    :initform nil
-	    :accessor subobjs
-	    :allocation :instance
-	    :documentation "Contains a (ordered?) list of chem-objs that make up this chem-obj (i.e. it is a higher level structure)"))
+(defclass/std chem-obj ()
+  ((subobjs :doc "Contains a (ordered?) list of chem-objs that make up this chem-obj (i.e. it is a higher level structure)")
+   (tfms :doc "A list of transformations (translations and rotations) The order is the first applied opperation is first in the list"))
   (:documentation "The base class used for containing chemical objects and the rule for maniipulating them. They can be used to create atomic (in the lisp sense of evaluating to themselves) level detail chemical objects such as atoms or course grain nucleotide models, which have well defined coordinate descriptions. Or chem-obj children can define higher level structures composed of atomic chem-obj or other higher level structures themselves, for example small molecules composed from atoms, DNA helical strands from nucleotides and Double helices from DNA helical strands."))
 
 
