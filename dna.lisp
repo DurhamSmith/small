ef561a30f9a13418993bda120effc745ec239abc
Durham <durham.smith@gmail.com>
2020-09-26 21:10:07 +0200

    add prev and next to DNA

--- dna.lisp
+++ dna.lisp
@@ -1,6 +1,8 @@
 (in-package :small)
 
-(defclass/std dna (chem-obj) ()
+(defclass/std dna (chem-obj)
+  ((prev :doc "The previous DNA CHEM-OBJ in dna sequence. Together with (next dna) form a doubly linked list of DNA CHEM-OBJS used to represent a grouped sequence of these DNA CHEM-OBJs, which themselves could be at various levels of abstraction (e.g strand composed of nucleotides, or origami scaffolds composed of single and helical strands). These sequences are on the same molecule, i.e. partner strands are not included")
+   (next :doc "The next DNA CHEM-OBJ in dna sequence. Together with (next dna) form a doubly linked list of DNA CHEM-OBJS used to represent a grouped sequence of these DNA CHEM-OBJs, which themselves could be at various levels of abstraction (e.g strand composed of nucleotides, or origami scaffolds composed of single and helical strands). These sequences are on the same molecule, i.e. partner strands are not included"))
   (:documentation "A class for DNA chem-objs. Defines constants and connect methods"))
 
 (describe 'dna)
