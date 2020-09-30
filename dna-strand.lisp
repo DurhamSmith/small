7db4779aacb06f099374604248a19ef64f0aabc5
Durham <durham.smith@gmail.com>
2020-09-30 17:26:07 +0200

    2

--- dna-strand.lisp
+++ dna-strand.lisp
@@ -10,3 +10,4 @@
    (seq :doc "The sequence of the strand (length must be equal to len)"))
   (:documentation "A CHEM-OBJ representing a DNA strand, mainly used as a parent for DNA-HELIX-STRAND and DNA-SINGLE-STRAND"))
 
+;;
