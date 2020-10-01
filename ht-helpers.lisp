1db09c2a5b7582e373eca67f92ce3e8108a897ce
Durham <durham.smith@gmail.com>
2020-10-01 12:44:35 +0200

    Add +ht T & I

--- ht-helpers.lisp
+++ ht-helpers.lisp
@@ -2,12 +2,30 @@
 
 (defun +ht (ht v &key k)
   "Adds v to ht under key k. If no k, k=(concatenate 'string (type-of v) #vs-in-ht)"
-  )
+  (let* ((k (if k
+		k
+		(concatenate 'string
+			 (clsnm-as-str v)
+			 " "
+			 (format nil "~A"
+				 (numtype-in-ht (type-of v) ht))))))
+    (setf (gethash k ht) v)))
 
 (defun numtype-in-ht (typ ht)
   "Returns the number of objects of type typ in ht"
-  (length (mapcar #'(lambda (v)
+  (let* ((l (length (mapcar #'(lambda (v)
 		      (typep v typ))
-		  (hash-table-values ht))))
+			   (hash-table-values ht)))))
+    (if l
+	l
+	0)))
+	
+	
   ;; (break "~A"  (length (maphash-values )
   ;; 				       ht))))
+
+
+(defun clsnm-as-str (x)
+    "Returns x's class-name as a sting"
+  ;;untested
+  (string (class-name (class-of x))))
--- tests/ht-helpers-tests.lisp
+++ tests/ht-helpers-tests.lisp
@@ -2,13 +2,13 @@
 
 (define-test "+ht"
   (let ((ht (make-hash-table))
-	(ans (list "DNA-NT 0" "DNA-NT 1" "DNA-NT 2"
-		   "DNA-NT 3" "DNA-NT 4" "DNA-NT 5")))
+	(ans (reverse (list "DNA-NT 0" "DNA-NT 1" "DNA-NT 2"
+		   "DNA-NT 3" "DNA-NT 4" "DNA-NT 5"))));; we reverese since we are not doing set comparison we are doing list compairison so order matters TODO add set comparison fns
     (loop for i from 0 to 5 do
-      (+ht ht (make-dna-nt))
+      (+ht ht (make-dna-nt)))
 ;;      (format t "~& ~A ~%" (alexandria:hash-table-keys ht))))
 
-      (is equal ans (alexandria:hash-table-keys ht)))))
+    (is equal ans (alexandria:hash-table-keys ht))))
 
 (define-test "numtype-in-ht (typ ht)"
   (let* ((ht (make-hash-table)))
@@ -18,4 +18,3 @@
 
   
 		       
-
