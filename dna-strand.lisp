11a5079393720b3b31a3aaae00a5aba5ba5b4acb
Durham <durham.smith@gmail.com>
2020-10-01 15:55:26 +0200

    add (add-nt strand) impl & (grow nt=int) impl. not tests for either

--- dna-strand.lisp
+++ dna-strand.lisp
@@ -11,12 +11,41 @@
   (:documentation "A CHEM-OBJ representing a DNA strand, mainly used as a parent for DNA-HELIX-STRAND and DNA-SINGLE-STRAND"))
 
 (defmethod connect ((o1 dna-strand) (o2 dna-strand) &rest rest)
-  "Sets (end o1) = (end o2) and connects their nucleotides"  
-  (dna-connect o1 o2))
+  
+  "Sets  o2:prev = o1, o1:next = o2 and connects their DNA-NTs"  
+  (dna-connect o1 o2)
+  (connect-nts (connected-nts o1) (connected o2)))
 
 (defun make-dna-strand (&rest rest)
   (make-instance 'dna-strand))
 
 
+(defgeneric grow (strand &key nts head)
+  (:documentation "Returns s after adding num nt to the 3' end of strand. 
+If head=t nt is added to the 5' end.
+If nt=nil the next DNA-NT is calculated via (next-nt s)")
+  (:method ((strand dna-strand) &key nts head)
+    (typecase nts
+      (integer (if (= 1 nts)
+		   (grow strand :head head)
+		   (progn
+		     (grow strand :head head)
+		     (grow strand :nts (- nts 1) :head head))))
+      (null (add-nt strand :head head))
+      (t (error "(grow strand) does not support type: ~A" nts)))))
 
+
+(defgeneric add-nt (strand &key nt head)
+  (:documentation "Returns (values strand nt) after adding DNA-NT nt to the 3' end of strand. If head=t adds it to the 5' end of strand")
+  (:method ((strand dna-strand &key nt head))
+    (typecase nt
+      (if head
+	  (connect-nts nt (nts strand))
+	  (connect-nts (nts strand) nt)))
+    (t (error "(grow strand) not supported for type: ~A" nt))))
+		     
+		  
+
+  
  
+(typep 'null nil)
