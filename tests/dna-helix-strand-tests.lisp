dc856a5a77374d349e80a18adf3349fffa0d77f7
Durham <durham.smith@gmail.com>
2020-10-06 09:53:50 +0200

    add (helix-strand) MANUAL TESTs

--- tests/dna-helix-strand-tests.lisp
+++ tests/dna-helix-strand-tests.lisp
@@ -38,6 +38,17 @@
 ;;   (write-oxdna nt :filename "row1_tmp"))
 
 
+
+
+
+(let* ((sc0 (v3l '(-30.5 0. -43.879999999999995)))
+       (st0 (v3l '(-32.36602540378444 -0.49999999999999994 -43.879999999999995)))
+       (ax0 (v3l '(-31.5 0 -43.879999999999995)))
+       (vbb (as-unit-vec (magicl:.- sc0 ax0)))
+       (cm (magicl:.+ ax0 (magicl:scale vbb 0.6d0)))
+       (vn (v3 0 0 1))
+       (hs (SMALL::helix-strand ax0 vn vbb 33)))
+  (write-oxdna (SMALL::5nt hs) :filename "helgun"))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 		; Tests creating and writing a partner strand ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
