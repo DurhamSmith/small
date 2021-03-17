9064ce50459c406ccb423f4d7dc06d845fe8557f
Durham <durham.smith@gmail.com>
2021-03-17 10:13:27 +0200

    Add tile with stap bridges to tutorial

--- examples/dna-array/part2.lisp
+++ examples/dna-array/part2.lisp
@@ -56,9 +56,10 @@
 
 ;; ![DNA Tile V0](tile-v0.png)
 
-# Adding Scaffold Bridges
-;;;; We see that the triangles (and the DNA-HELIX-STRAND and DNA-NTs) have been rotated. Notice how we have retrieved each individual triangle from the tile and written it out. This is because currently the triangle are disconnected entities. We fix this by adding each triangle to the tiles scaffold using the add-to-scaffold function provided by the dna-origami class (that we used as a superclass when creating the tile). Again we use the add-to-scaffold function introduced in part 1.
-       
+;;;; # Adding Scaffold Bridges
+;; We see that the triangles (and the `dna-helix-strand`s and `dna-nt`s they contain) have been rotated. Notice how we have retrieved each individual triangle from the tile and written it out. This is because currently the triangle are disconnected entities. We fix this by adding each triangle to the `dna-tile`s scaffold using the `add-to-scaffold` function (introduced in part 1) provided by the `dna-origami` class (that we used as a superclass when creating the tile). We redefine the `initialize-instance` function 
+;; specialized on `dna-tile` so that the `dna-triangle`s are added to the scaffold when we create an instance of `dna-tile` instead of having to do it manually every time.
+
 (defmethod initialize-instance :after ((obj dna-tile) &key)
   (with-accessors ((t1 t1) (t2 t2) (t3 t3) (t4 t4)) obj
     (let* ((rot90 (rotation-matrix (v3 0 1 0) (/ pi -2)))
@@ -73,10 +74,13 @@
     (add-to-scaffold obj t4)))
 
 
-;;;; This has set the 5nt and 3nt of the tile, as well as connected the ith triangle to the i+1th as they were added to the scaffold. Again we can write this to a file and see the results.
+;; This has set the `5nt` and `3nt` of the `dna-tile`, as well as connected the ith triangle to the i+1th when they were added to the scaffold. Again we can write this to a file and see the results, this time without needing to retrieve all the `dna-triangle`s individually.
 (wmdna "tile-v1"  (make-instance 'dna-tile))
-p
-;;;; All looks good! Or does it? We have forgotten to add the scaffold bridges that join triangle i to triangle i+1. Not to worry this is easily fixed. Again we will use the bridging-single-strand function, this time explicitly passing the number of nucleotide to be 10 as in the paper (see SI p9). We create a function to do this, 
+;; ![DNA Tile V1](tile-v1.png)
+
+;; All looks good! Or does it? When we closely examine the connections between `dna-triangles` we notice have forgotten to add the scaffold bridges that join `dna-triangle` i to `dna-triangle` i+1. Not to worry this is easily fixed. Again we will use the `bridging-single-strand` function (introduced in part 1), this time explicitly passing the number of nucleotides to be 10 (as in the paper see SI p9). Lets create a function to do this.
+
+;; First we define a utility function to retun `dna-triangle` `k` when the `dna-tile` and triangle index `k` (1-4)
 
 (defun get-triangle (tile k)
   "Utility function to retrieve triangle k from the tile"
@@ -86,6 +90,8 @@ p
 	((= k 4) (t4 tile))
 	(t (error "k=~A is not a valid triangle index"))))
 
+;; Now we can define the function to create the scaffold bridge. Its arguments are a `dna-tile` and triangle index `k` and returns a `dna-single-strand` the spans the distance from the 3' end of the kth triangle to the 5' end of the k+1th triangle.
+
 (defun tile-stap-bridge (tile k)
   "Returns a DNA-SINGLE-STRAND that connects triangle k to triangle k+1 in the tile"
   (let* ((tk (get-triangle tile k))
@@ -94,11 +100,13 @@ p
 	 (axis-k+1 (axis (5nt tk+1)))) ;; Retrive the helix axis coords of first nucleotide in triangle k+1
     (bridging-single-strand axis-k axis-k+1 (v3 0 1 0) :len 10)))
 
-;;;; In this we used the function axis which takes a DNA-NT and returns the coordinates of its axis. small provides many such function for the retireval of geometric points, one can look at the dna.lisp file to find more such functions. Lets check that this works as expected
+;; In defining `tile-stap-bridge` we used the function `axis` which takes a `dna-nt` and returns the coordinates of its axis. `small` provides many such function for the retrieval of geometric points, one can look at the [dna.lisp](https://github.com/DurhamSmith/small/blob/master/dna.lisp) file to find more such functions. Lets check that this works as expected
 
-(tile-stap-bridge (make-instance 'dna-tile) 1)
+;; (tile-stap-bridge (make-instance 'dna-tile) 1)
 
-;;;; We see we get DNA-SINGLE-STRAND and that there are 10 nucleotides as expected. Now lets modify our initialization of the DNA-TILE
+;; We see that `tile-stap-bridge` makes use of Common Lisps ability to return multiple value. It returns a  `DNA-SINGLE-STRAND` and a list of the `DNA-NT`s that are contained in it. We see that there are 10 DNA as expected.
+
+;; Now lets modify our initialization of the `dna-tile` to add the scaffold bridges to our scaffold.
 
 	 
 (defmethod initialize-instance :after ((obj dna-tile) &key)
@@ -117,12 +125,13 @@ p
     (add-to-scaffold obj (tile-stap-bridge obj 3))
     (add-to-scaffold obj t4)))
 
-;;;; Now we can write the new tile out and check that everything works as expected
+;; Let write out the `dna-tile` and see thta the scaffold bridges are included.
 (wmdna "tile-v2"  (make-instance 'dna-tile))
+;; ![DNA Tile V2](tile-v2.png)
+;; Great! The staple bridges are included and everything is as expected
 
-;;;; We see that the scaffold bridges are included. TODO ADD FIG
-
-;;;; Next we want to add some staple bridges to hold the tile together. To do so we will define a function that take a dna-tile the tile number k [1,4] and a helix number i [12,22] and keyword arguments :l1 and :l2 returns a staple that between the kth triangle ith helix and the k+1th triangle 23-ith helix.
+# Adding Staples
+Next we want to add some staple bridges to hold the tile together. To do so we will define a function that take a dna-tile the tile number k [1,4] and a helix number i [12,22] and keyword arguments :l1 and :l2 returns a staple that between the kth triangle ith helix and the k+1th triangle 23-ith helix.
 
 (find-obj-with-props (scaffold (t1 (make-instance 'dna-tile))) '((:i . 1)))
 
