64524cefe0aa4bbc34bf47757c868574fb7ae5a7
Durham <durham.smith@gmail.com>
2021-04-01 18:49:58 +0200

    Add brief example of getting s3 folder info in python

---
+++ small-repo/s3.py
@@ -0,0 +1,18 @@
+import boto3
+# s3 = boto3.resource('s3')
+# # for bucket in s3.buckets.all():
+# #     print(bucket.name)
+
+# obj = s3.Object("zs3-demo", "1/info")
+# print(obj.get()['Body'].read().decode('utf-8'))
+
+s3 = boto3.client("s3")
+all_objects = s3.list_objects(Bucket = 'zs3-demo', Delimiter='/')
+
+s3 = boto3.resource('s3')
+pres = all_objects['CommonPrefixes']
+for pre in pres:
+    filename = f'{pre["Prefix"]}info'
+    obj = s3.Object("zs3-demo", filename)
+#    print(filename)
+    print(obj.get()['Body'].read().decode('utf-8'))
