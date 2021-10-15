deb574c76a6d07b0e9ff0f7066cf33658b71f7e8
Durham <durham.smith@gmail.com>
2021-10-15 13:38:09 +0200

    add basic utils calling function

--- src/oxdna/oxdna.lisp
+++ src/oxdna/oxdna.lisp
@@ -1,8 +1,20 @@
+(defparameter *oxdna-path* "/home/dd/PhD/Software/oxDNA/")
+(defparameter *oxdna-exe-path* "/home/dd/PhD/Software/oxDNA/build/bin/oxDNA")
+(defparameter *oxdna-utils-path* "/home/dd/PhD/Software/oxDNA/UTILS/")
+
+
+
+(defun set-oxdna-path (oxdna-path)
+  (setf *oxdna-path* oxdna-path
+        *oxdna-exe-path* (concatenate 'string oxdna-path "/bulid/bin/oxDNA")
+        *oxdna-utils-path* (concatenate 'string oxdna-path "/UTILS/")))
+
+
 (defun run-oxdna (input-file)
   "runs an oxdna sim"
   (let ((shell (uiop:launch-program "bash" :input :stream :output :stream)))
     (write-line (concatenate 'string
-                             "oxDNA "
+                            *oxdna-exe-path* " "
                              input-file)
                 (uiop:process-info-input shell))
     (force-output (uiop:process-info-input shell))))
@@ -16,3 +28,14 @@
                 (format stream "~A  = ~A ~%" (first key-val) (second key-val)))
             (group options-pairs 2)))
   filename)
+
+(run-oxdna-util output_bonds.py <input_file> <trajectory_file> [counter])
+
+(defmacro run-oxdna-util (&rest rest)
+  (let ((command (concatenate 'string
+                              *oxdna-utils-path*
+                              (format nil "~{ ~a~}" rest)))
+        (shell (uiop:launch-program "bash" :input :stream :output :stream)))
+    (write-line command
+                (uiop:process-info-input shell))
+    (force-output (uiop:process-info-input shell))))
