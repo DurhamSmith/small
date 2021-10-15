5180ce8b54e3f783693cae66c831d4f759fd52d0
Durham <durham.smith@gmail.com>
2021-10-15 13:01:10 +0200

    Add open-in-oxview function

--- src/oxviewer/oxviewer.lisp
+++ src/oxviewer/oxviewer.lisp
@@ -5,3 +5,6 @@
 (defun show-in-oxview (filename &rest dna)
   (write-as "oxdna" filename dna)
   (uiop:run-program (concatenate 'string "oxview " filename ".top " filename ".oxdna")))
+
+(defun open-in-oxview (filename &key (conf-ext ".oxdna") (top-ext ".top"))
+  (uiop:run-program (concatenate 'string "oxview " filename top-ext filename conf-ext)))
