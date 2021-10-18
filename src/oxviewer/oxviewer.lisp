383d47ceeb395392ece625d26521f34a4992a008
Durham <durham.smith@gmail.com>
2021-10-18 12:59:16 +0200

    add oxview traj fn

--- src/oxviewer/oxviewer.lisp
+++ src/oxviewer/oxviewer.lisp
@@ -6,8 +6,11 @@
   (write-as "oxdna" filename dna)
   (uiop:run-program (concatenate 'string "oxview " filename ".top " filename ".oxdna")))
 
-(defun open-in-oxview (filename &key (conf-ext ".oxdna") (top-ext ".top"))
+(defun open-in-oxview (filename &key (conf-ext ".oxdna") (top-ext ".top") )
   (uiop:run-program (concatenate 'string "oxview " filename top-ext " " filename conf-ext)))
 
 (defun oxview ()
   (uiop:run-program "oxview"))
+
+(defun oxview-trajectory (topfile traj-file)
+  (uiop:run-program (format nil "oxview ~A ~A" topfile traj-file)))
